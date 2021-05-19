7c138e999cb644047a97496563fa33692251baaa
Pham Long Huan <huanvip455@gmail.com>
2021-05-19 23:00:24 +0700

    card is designed

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -5,7 +5,11 @@ class Card extends React.Component {
     render() {
         return (
             <div className="card">
-
+                <img src={this.props.imgLink} />
+                <p>{this.props.children}</p>
+                <div className="card-btn-container">
+                    <button className="btn--card">Go Somewhere</button>
+                </div>
             </div>
         )
     }
