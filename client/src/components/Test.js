d4b43e7117044ab4ead4911048f83cd18374a847
Pham Long Huan <huanvip455@gmail.com>
2021-05-19 16:49:12 +0700

    test finish

---
+++ client/src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Test.scss';
+
+class Test extends React.Component {
+    render() {
+        return (
+            <h1>Hello Huan</h1>
+        )
+    }
+}
+
+export default Test;
